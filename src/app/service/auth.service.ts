b6eab57fd1b29871154cc8fcb4119b551fdb7468
amankr8 <amankumar141099@gmail.com>
2025-02-01 22:27:50 +0530

    env var added

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://mercheazy-server.onrender.com/api/auth';
+  private apiUrl = environment.baseUrl + '/api/auth';
 
   constructor(private http: HttpClient) {}
 
