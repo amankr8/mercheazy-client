2e41e8b4ca1ac8e8a8d670d6e204317b08882612
amankr8 <amankumar141099@gmail.com>
2025-03-04 23:10:53 +0530

    login loading added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: import.meta.env['NG_APP_BASE_URL'] || 'http://localhost:8080',
+  baseUrl: import.meta.env['NG_APP_BASE_URL'] || 'http://localhost:8080/',
 };
