db7bb0f7c03212ef68b4314d1fb57cfd849d2321
amankr8 <amankumar141099@gmail.com>
2025-03-23 15:19:14 +0530

    code refactor

--- src/app/pages/landing/home/home.component.ts
+++ src/app/pages/landing/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, RouterModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
